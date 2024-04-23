0f593e73c231947d5a9f04703b7ef475bef2c4b2
MasamichiKanakubo <renlijinjiubao808@gmail.com>
2024-04-23 10:10:32 +0900

    feat: set middleware and root EP

--- app/main.py
+++ app/main.py
@@ -1,4 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def hello():
+    return {"message": "Hello World"}
