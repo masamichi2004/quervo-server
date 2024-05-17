dfa287f51b513eea17f51af911ef9fd087573cb1
umiyuri777 <yuseiai15@gmail.com>
2024-05-17 17:30:17 +0900

    HuggingFaceEmbeddingsでベクトル化するようにした

--- tests/langchain/test_langchain.py
+++ tests/langchain/test_langchain.py
@@ -1,9 +1,7 @@
-from openai import OpenAI 
-import os
 from dotenv import load_dotenv
 from langchain_community.document_loaders import CSVLoader
-from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from pprint import pprint
 
 CHUNK_SIZE = 256
@@ -13,28 +11,32 @@ COLLECTION_NAME = "langchain"
 
 
 # CSVファイルからドキュメントを読み込む
-loader = CSVLoader(
-    file_path="../../app/data/example.csv",
-    csv_args={
-        "delimiter": ",",
-        "quotechar": '"',
-        "fieldnames": ["id", "name", "long", "lat", "area", "category"],
-    },
-    encoding="utf-8",
-)
+print("CSVファイルからドキュメントを読み込む")
+try:
+    loader = CSVLoader(
+        file_path="../../app/data/example.csv",
+        csv_args={
+            "delimiter": ",",
+            "quotechar": '"',
+            "fieldnames": ["id", "name", "long", "lat", "area", "category"],
+        },
+        encoding="utf-8",
+    )
 
-# 環境変数の読み込み
-load_dotenv()
-api_key = os.getenv("API_KEY")
+    # 変数docsにドキュメントを読み込む
+    docs = loader.load()
 
-# 変数docsにドキュメントを読み込む
-docs = loader.load()
+except Exception as e:
+    print(f"エラー: {e}")
 
-# OpenAIのテキスト埋め込みモデルを使用
-embedding = OpenAIEmbeddings(openai_api_key = api_key ,model="text-embedding-ada-002")
+
+# 埋め込み表現生成用モデルをHugging Face Hubから取得
+embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/distiluse-base-multilingual-cased-v2")
 
 # Chromaデータベースを生成するディレクトリを指定
 persist_directory = "../../app/vectordb"
+
+print("ドキュメントをChromaデータベースに格納")
 
 # ドキュメントをChromaデータベースに格納
 vectordb = Chroma.from_documents(
@@ -42,7 +44,10 @@ vectordb = Chroma.from_documents(
     embedding=embedding,
     collection_name="faqs",
     persist_directory=persist_directory,
+    collection_metadata={"hnsw:space": "cosine"}
 )
+
+print("ベクトル情報のデータベースを作成しました")
 
 query = "一人でゆっくり飲みたいなあ"
 
