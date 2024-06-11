022a2bfafaccf55468c302d7149b90733f228983
umiyuri777 <yuseiai15@gmail.com>
2024-06-11 23:46:54 +0900

    fix: 元に戻した

--- app/models/api_models.py
+++ app/models/api_models.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel
-from typing import Optional
 from models.coordinate import Coordinate
 class Prompt(BaseModel):
-    current_coodinate: Optional[Coordinate] = None     # type: Coordinate
+    current_coodinate: tuple[float, float]     # type: Coordinate
     prompt:  str    # type: str   ～したいなどの文字列
