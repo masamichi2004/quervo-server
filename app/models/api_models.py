9805ad10051de7b68d90acb13addb8478c193966
umiyuri777 <yuseiai15@gmail.com>
2024-06-12 21:08:01 +0900

    refactor: current_coodinateの型をtupleからOptional[Coordinate]に変更

--- app/models/api_models.py
+++ app/models/api_models.py
@@ -1,4 +1,6 @@
 from pydantic import BaseModel
+from typing import Optional
+from app.models.coordinate import Coordinate
 class Prompt(BaseModel):
-    current_coodinate: tuple[float, float]     # type: Coordinate
-    prompt:  str    # type: str   ～したいなどの文字列
+    current_coodinate: Optional[Coordinate] = None     # type: Coordinate
+    prompt:  str
